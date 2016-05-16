cb7cc9c9556f69cdd06f5a323bd1ab4024c88804
Sarath KCM <sarathkcm@gmail.com>
2016-05-17 00:53:44 +0530

    Changing code to suit node

--- README.md
+++ README.md
@@ -1,45 +1,10 @@
-tmdb-js
+TheMovieDB-Client
 =======
 
-A simple JS object to call the API of TheMovieDB.  
+A simple node module to make calls to TheMovieDB API, based on EtienneWan's tmdb-js project.  
 
 Get Started
 -----------
 
 First, insert the script into your HTML file :
-```html
-<script type="text/javascript" src="/path/to/tmdb.js"></script>
-```
-Set your api_key on line 4.  
-(You can make calls to the API with SSL/TLS by modifying the uri to begin with https:// instead of http://)  
-
-Test the script by making a simple call with the console :
-```js
-tmdb.call("/configuration", {},
-  function(e){
-    console.log("Success: "+e)
-  }, 
-  function(e){
-    console.log("Error: "+e)
-  }
-)
-```
-It should always return a JSON object. If you get the error callback, then it's probably that you made a mistake in your api_key or in the url (first argument of tmdb.call).
-
-Usage
------
-```js
-tmdb.call(url, params, success, error)
-```
-url (String) : The method you want to call, beginning with "/". [See all methods here](http://docs.themoviedb.apiary.io/ "Going to APIary.io")  
-params (Object) : Set your parameters here. Api_key is already included, no need to add it again ;) For example (calling method /search/movie) :
-```json
-{
-  "query": "The Hitchhiker's Guide to the Galaxy",
-  "year": 2005,
-  include_adult: false
-  
-}
-```
-success & error (Functions) : Callbacks with one argument to get the data, already parsed as JSON.
-
+http://docs.themoviedb.apiary.io/ "Going to APIary.io"
---
+++ themoviedb-client.js
@@ -0,0 +1,93 @@
+(function () {
+    var http = require('http');
+    var tmdb = function (key) {
+        if (!key)
+            throw "Invalid API Key";
+        var api_key = key;
+        var settings = {
+            "host": "api.themoviedb.org",
+            "path": "/3",
+            "images_url": "http://image.tmdb.org/t/p",
+            "timeout": 5000,
+            "update_images_url": true
+        };
+
+        var tmdbConfig = null;
+
+        this.prototype.updateApiConfiguration = function () {
+            if (!tmdbConfig && settings.update_images_url) {
+                tmdbConfig = {};
+                this.call("/configuration", {}).then(function (response) {
+                    settings.images_uri = response.images.base_url;
+                    tmdbConfig = response;
+                }, function (e) {
+                    tmdbConfig = null;
+                });
+            }
+        };
+
+        this.prototype.configure = function (options) {
+            settings.host = options.host || settings.host;
+            settings.path = options.path || settings.path;
+            settings.images_url = options.host || settings.images_url;
+            settings.timeout = options.timeout || settings.timeout;
+            settings.update_images_url = options.update_images_url || settings.update_images_url;
+        };
+
+        this.prototype.call = function (url, params) {
+            this.updateApiConfiguration();
+            var params_str = "api_key=" + api_key;
+            for (var key in params) {
+                if (params.hasOwnProperty(key)) {
+                    params_str += "&" + key + "=" + encodeURIComponent(params[key]);
+                }
+            }
+            return new Promise(function (resolve, reject) {
+                try {
+                    var httpOptions = {
+                        host: settings.host,
+                        path: settings.path + url + "?" + params_str,
+                        headers: {
+                            'Accept': 'application/json'
+                        },
+                        method: 'GET'
+                    };
+
+                    var request = http.request(httpOptions, function (response) {
+
+                        var res = '';
+
+                        response.on('data', function (chunk) {
+                            res += chunk;
+                        });
+
+                        response.on('end', function () {
+                            resolve(JSON.parse(res));
+                        });
+
+                    });
+
+                    request.setTimeout(tmdb.timeout, function () {
+                        reject("Request timed out: " + settings.path + url + "?" + params_str);
+                    });
+
+                    request.on('error', function (err) {
+                        reject(err);
+                    });
+
+                    request.end();
+                }
+                catch (err) {
+                    reject(err);
+                }
+            });
+        };
+        this.prototype.getImageUrl = function (filename, size) {
+            this.updateApiConfiguration();
+            return tmdb.images_uri + size + filename;
+        };
+
+    };
+
+    module.exports = tmdb;
+})();
